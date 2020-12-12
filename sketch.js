d8dad8c33c9c0e862210fd3e70e3f5fa35a014f4
Your Name <you@example.com>
2020-12-12 15:37:54 +0530

    Vanishing pigs

--- sketch.js
+++ sketch.js
@@ -21,20 +21,20 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box1 = new Box(740,320,70,70);
+    box2 = new Box(880,320,70,70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log1 = new Log(810,260,210, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+    box3 = new Box(740,240,70,70);
+    box4 = new Box(880,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 =  new Log(810,180,210, PI/2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    box5 = new Box(805,180,60,60);
+    log4 = new Log(770,120,115, PI/7);
+    log5 = new Log(850,120,115, -PI/7);
 
     bird = new Bird(200,50);
 
@@ -74,4 +74,10 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode===32){
+        slingshot.attach(bird.body);
+    }
 }
